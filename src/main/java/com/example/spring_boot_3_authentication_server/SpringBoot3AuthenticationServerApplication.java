05d05ea6f067b11ef195b44f79e4caf3da62f820
Anthony <anthonypanont@gmail.com>
2025-02-06 16:57:30 +0100

    Init (yaml, .properties et git Init)

---
+++ src/main/java/com/example/spring_boot_3_authentication_server/SpringBoot3AuthenticationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_3_authentication_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3AuthenticationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3AuthenticationServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_3_authentication_server/SpringBoot3AuthenticationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_3_authentication_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3AuthenticationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
